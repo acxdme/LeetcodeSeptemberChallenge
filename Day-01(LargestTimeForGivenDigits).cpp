b5d714af40ddba97a72384b7e5df3263f5b1ecdf
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-09-02 11:33:17 +0530

    Create Day-01(LargestTimeForGivenDigits).cpp

---
+++ Day-01(LargestTimeForGivenDigits).cpp
@@ -0,0 +1,29 @@
+/*
+Time complexity : O(1)
+space complexity : O(1)
+My approach : go through all the combinations and find the maximum possible time which is in valid format.
+*/
+
+class Solution {
+public:
+      string largestTimeFromDigits(vector<int>& A) {
+        string result;
+        for(int i = 0; i < 4; ++i)
+        {
+            for(int j = 0; j < 4; ++j)
+            {
+                for(int k = 0; k < 4; ++k)
+                {
+                    if(i == j || j == k || k == i)
+                        continue;
+                    string hh = to_string(A[i]) + to_string(A[j]);
+                    string mm = to_string(A[k]) + to_string(A[6-i-j-k]);
+                    string _time = hh + ":" + mm;
+                    if(hh < "24" && mm < "60" && _time > result)
+                        result = _time;
+                }
+            }
+        }
+        return result;
+    }
+};
