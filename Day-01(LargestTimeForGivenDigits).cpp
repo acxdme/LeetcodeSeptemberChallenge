80f7fdf379a78ddc0f31fb33a08383dacc085df6
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-09-02 14:36:53 +0530

    Update Day-01(LargestTimeForGivenDigits).cpp

--- Day-01(LargestTimeForGivenDigits).cpp
+++ Day-01(LargestTimeForGivenDigits).cpp
@@ -3,7 +3,56 @@ Time complexity : O(1)
 space complexity : O(1)
 My approach : go through all the combinations and find the maximum possible time which is in valid format.
 */
+//Method-1
+class Solution {
+public:
+    
+    set<string> st;
+     void allPermutations(string a,int l, int r)
+     {
+         if(l==r)
+         {
+            
+            st.insert(a);
+         }
+         else
+         {
+             for(int i=l;i<=r;i++)
+             {
+                 swap(a[l],a[i]);
+                 
+                 allPermutations(a,l+1,r);
+                 
+                 swap(a[l],a[i]);
+             }
+         }
+     }
+    
+    string largestTimeFromDigits(vector<int>& A) {
+        string ans;
+        
+        if(A[0]>2 and A[1]>2 and A[2]>2 and A[3]>2)
+            return ans;
+        
+        string imm = to_string(A[0]) +to_string(A[1]) +to_string(A[2]) +to_string(A[3]);
+         allPermutations(imm, 0, 3);
+        
+        string result;
+        for(string it : st)
+        {
+            if(it<= "2359" and it>result and it[2] < '6')
+                result =it; 
+        }
+         if(result.size()==4)
+         result += ':';
+         swap(result[4],result[3]);
+         swap(result[3],result[2]);
 
+        return result;
+    }
+};
+
+//Method-2
 class Solution {
 public:
       string largestTimeFromDigits(vector<int>& A) {
